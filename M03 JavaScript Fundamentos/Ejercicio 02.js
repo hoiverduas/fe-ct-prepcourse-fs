0c3ea0bba98dbb289d729ee75c90ad4cbfdc8a92
hoiverduas <hoiver75@gmail.com>
2023-09-23 20:56:38 -0500

    Se realiza módulo 3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,33 +4,44 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
-
+devolverString('h');
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var result = (x + y);
+   return result;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var result = (x - y);
+   return result;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var result = (x / y);
+   return result;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var result = (x * y);
+   return result;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var result = (x % y);
+   return result;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
